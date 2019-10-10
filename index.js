8315e24afc207b446d0af2887f4b59364f886a14
Valter Kraemer <valterkraemer@users.noreply.github.com>
2019-10-10 09:30:32 +0300

    show speed

--- index.js
+++ index.js
@@ -5,6 +5,23 @@
 
   window.videoSpeedInit = true;
 
+  const element = document.createElement("div");
+
+  Object.assign(element.style, {
+    backgroundColor: "#CCC",
+    color: "#000",
+    position: "fixed",
+    top: "0px",
+    right: "0px",
+    padding: "8px 12px",
+    borderBottomLeftRadius: "5px",
+    fontFamily: "Helvetica",
+    fontSize: "16px",
+    fontWeight: "normal"
+  });
+
+  let timer;
+
   let speed = 0;
   let dotActive = false;
 
@@ -24,9 +41,22 @@
         speed = newSpeed;
       }
 
+      show(newSpeed);
       document.querySelector("video").playbackRate = newSpeed;
     }
 
     dotActive = false;
   });
+
+  function show(speed) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    element.innerText = `${speed}x`;
+    document.body.appendChild(element);
+
+    timer = setTimeout(() => {
+      document.body.removeChild(element);
+    }, 2000);
+  }
 })();
